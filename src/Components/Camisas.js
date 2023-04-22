597977cdfab4719bcf975644e1c04835e89c2e2d
vinnybrito <viny.rbrito15@hotmail.com>
2023-04-22 00:36:41 -0300

    melhorias e estilação da página camisas

--- src/Components/Camisas.js
+++ src/Components/Camisas.js
@@ -1,24 +1,159 @@
 import '../index.css'
 import '../Components/css/style.css'
+import {useState,useEffect} from 'react'
+import camisa01 from './img/camisa01.png'
+import camisa02 from './img/camisa02.png'
+import camisa03 from './img/camisa03.png'
 
 export default function Camisas() {
+
+    const [count,setCount] = useState(0);
+
+    useEffect(() => {
+        fetch()
+    },[])
+
     return(
         <div id='camisas'>
 
             <h1 id='camisasM'>Masculino</h1>
 
+            {/*-----------------------PRIMEIRA SEÇÃO-----------------------*/}
+
             <section id='sec01'>
 
                 <article className='ctn'>
-                    <h1>helloo</h1>
+
+                    <div>
+                        <img src={camisa01} alt="camisa01" />
+                    </div>
+
+                    <div className='info'>
+
+                    </div>
+
+                    <div>
+                        <button className='btn-click' onClick={() => setCount(count +1)}>+</button>
+
+                        <input type="text" placeholder={count}/>
+
+                        <button className='btn-click' onClick={() => setCount(count -1)}>-</button>
+                    </div>   
+                    <div>
+                        <input type="submit" value="Comprar" className="classComprar"></input>
+                    </div>  
                 </article>
 
                 <article className='ctn'>
-                    <h1>oi</h1>
+                    <div>
+                        <img src={camisa02} alt="camisa01" />
+                    </div>
+
+                    <div className='info'>
+
+                    </div>
+
+                    <div>
+                        <button className='btn-click' onClick={() => setCount(count +1)}>+</button>
+
+                        <input type="text" placeholder={count}/>
+
+                        <button className='btn-click' onClick={() => setCount(count -1)}>-</button>
+                    </div>   
+                    <div>
+                        <input type="submit" value="Comprar" className="classComprar"></input>
+                    </div>
                 </article>
 
                 <article className='ctn'>
-                    <h1>hey</h1>
+                    <div>
+                        <img src={camisa03} alt="camisa01" />
+                    </div>
+
+                    <div className='info'>
+
+                    </div>
+
+                    <div>
+                        <button className='btn-click' onClick={() => setCount(count +1)}>+</button>
+
+                        <input type="text" placeholder={count}/>
+
+                        <button className='btn-click' onClick={() => setCount(count -1)}>-</button>
+                    </div>   
+                    <div>
+                        <input type="submit" value="Comprar" className="classComprar"></input>
+                    </div>
+                </article>
+
+            </section>
+
+            {/*-----------------------SEGUNDA SEÇÃO-----------------------*/}
+
+            <section id='sec01'>
+
+                <article className='ctn'>
+
+                    <div>
+                        <img src={camisa01} alt="camisa01" />
+                    </div>
+
+                    <div className='info'>
+
+                    </div>
+
+                    <div>
+                        <button className='btn-click' onClick={() => setCount(count +1)}>+</button>
+
+                        <input type="text" placeholder={count}/>
+
+                        <button className='btn-click' onClick={() => setCount(count -1)}>-</button>
+                    </div>   
+                    <div>
+                        <input type="submit" value="Comprar" className="classComprar"></input>
+                    </div>  
+                </article>
+
+                <article className='ctn'>
+                    <div>
+                        <img src={camisa02} alt="camisa01" />
+                    </div>
+
+                    <div className='info'>
+
+                    </div>
+
+                    <div>
+                        <button className='btn-click' onClick={() => setCount(count +1)}>+</button>
+
+                        <input type="text" placeholder={count}/>
+
+                        <button className='btn-click' onClick={() => setCount(count -1)}>-</button>
+                    </div>   
+                    <div>
+                        <input type="submit" value="Comprar" className="classComprar"></input>
+                    </div>
+                </article>
+
+                <article className='ctn'>
+                    <div>
+                        <img src={camisa03} alt="camisa01" />
+                    </div>
+
+                    <div className='info'>
+
+                    </div>
+
+                    <div>
+                        <button className='btn-click' onClick={() => setCount(count +1)}>+</button>
+
+                        <input type="text" placeholder={count}/>
+
+                        <button className='btn-click' onClick={() => setCount(count -1)}>-</button>
+                    </div>   
+                    <div>
+                        <input type="submit" value="Comprar" className="classComprar"></input>
+                    </div>
                 </article>
 
             </section>
