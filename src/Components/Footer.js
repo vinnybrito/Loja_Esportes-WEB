3a7c1437ca220481f9bec3b28bffc76036c53c7b
ViniKot <vini.kotches@gmail.com>
2023-04-27 13:43:13 -0300

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,17 +3,6 @@ import '../Components/css/style.css'
 
 export default function Footer() {
     return(
-        <div id="footer">
-            <div>
-                <p className='p-footer'>
-                    Vinicius Rodrigues Brito - RM: 97473
-                </p>
-            </div>
-            <div>
-                <p className='p-footer'>
-                    Vinícius Almeida Kotchetkoff - RM: 96331
-                </p>
-            </div>         
-        </div>
+        
     )
 }
