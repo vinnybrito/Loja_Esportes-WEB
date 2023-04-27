8933306f6d0e0f9ee855d40d3d38b22f81336a6c
ViniKot <vini.kotches@gmail.com>
2023-04-27 13:45:25 -0300

    calcas

--- src/Components/Calcas.js
+++ src/Components/Calcas.js
@@ -4,7 +4,11 @@ import imgConstrucao from './img/SiteContrucao.jpg'
 
 export default function Calcas() {
     return(
-        
-        
+        <div id='calcas'>
+            <p>Página e construção</p>
+            <div>
+                <img src={imgConstrucao} alt="Site em construcao" className='siteConstrucao' />
+            </div>
+        </div>
     )
 }
